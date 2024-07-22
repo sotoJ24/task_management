d5c0ddc1e8d236849fe5d94d9b47db564a088d2f
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-07-21 23:11:27 -0600

    Update HouseSeeder.php

--- database/seeders/HouseSeeder.php
+++ database/seeders/HouseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
-use App\Models\House;
+use App\Models\House; 
 
 class HouseSeeder extends Seeder
 {
